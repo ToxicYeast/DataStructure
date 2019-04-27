5c2df64f0aa1c297d33a00277a4f314c5d62a8ed
ToxicYeast <49139575+ToxicYeast@users.noreply.github.com>
2019-04-27 15:30:51 +0900

    Add files via upload

--- LinkedList/LinkedListTest.java
+++ LinkedList/LinkedListTest.java
@@ -2,12 +2,14 @@ public class LinkedListTest {
 	public static void main(String[] args) {
     	
     	// int sizeOf() : return LinkedList current size
+    	// int search() : return index number of typical vaule in LinkedList ; -1 : not found
     	// void printList() : print node vaule in current LinkedList
     	// void insertFront(int input) : insert a value into LinkList head
     	// void insertBack(int input) : insert a value into LinkedList bottom
     	// void insertMid(int input , int index) : insert a value into LinkedList (after n-th Node)
     	// void remove(int index) : remove n-th Node
     	// void reverse() : reverse the LinkedList sequence
+    	
     	
     	
     	LinkedList list = new LinkedList();
@@ -22,6 +24,7 @@ public class LinkedListTest {
     	list.printList();						// "1 3"
     	list.reverse();							// 3 1
     	list.printList();						// "3 1"
+    	System.out.println(list.search(5));		// "1"
     	
 	}
 }
